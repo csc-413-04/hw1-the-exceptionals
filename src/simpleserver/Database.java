29974f0a510af9ce5786e3c9d8c3c33ee58afca5
unknown <nbullock@mail.sfsu.edu>
2018-09-28 11:54:11 -0700

    Loop added; needs testing

--- src/simpleserver/Database.java
+++ src/simpleserver/Database.java
@@ -2,26 +2,38 @@ package simpleserver;
 
 import com.google.gson.*;
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
 
     private HashMap<String, User> userHashMap = new HashMap<>();
     private HashMap<String, Posts> postsHashMap = new HashMap<>();
 
-    public Database() throws Exception{
+    public Database() throws FileNotFoundException, UnsupportedEncodingException {
 
         InputStream dataStream = new FileInputStream("./Data/data.json");
         JsonParser parser = new JsonParser();
         JsonElement parsedObject = parser.parse(new InputStreamReader(dataStream, "UTF-8"));
         JsonObject baseObject = parsedObject.getAsJsonObject();
-        JsonArray userArray = new JsonArray();
-        JsonArray postArray = new JsonArray();
+        JsonArray userArray = baseObject.getAsJsonArray();
+        JsonArray postArray = baseObject.getAsJsonArray();
 
-        /*
-        To be added: loops and/or methods to add objects contained within the baseObject JsonObject to convert
-        to string and add them to the proper HashMap
-         */
+        //The following loop appears functional, but needs to be tested.
+        //In particular, the UserUsername and UserID strings may not be correct.
+
+        //This for loop setup causes the loop to iterate through each object in the array.
+        for(JsonElement retrievedUser : userArray){
+            JsonObject retrievedUserObject = retrievedUser.getAsJsonObject();
+
+            //The following two lines retrieve the parts of the JsonObject with the corresponding member names.
+            //It may not be fully functional and will await a proper test.
+            String UserUsername = retrievedUserObject.get("username").getAsString();
+            String UserID = retrievedUserObject.get("userID").getAsString();
+            int userIDint = Integer.parseInt(UserID);
+            User userObject = new User(userIDint, UserUsername);
+            userHashMap.put(UserUsername, userObject);
+        }
 
 
     }

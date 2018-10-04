35210ddee8524816cf35bfbaba1d2b8063ea1928
Edwin J Menjivar <edwinm.csm1ke@gmail.com>
2018-10-03 17:34:54 -0700

    Parser Update

--- src/simpleserver/Parser.java
+++ src/simpleserver/Parser.java
@@ -15,77 +15,86 @@ public class Parser extends Database {
         // output should be like endpoints-users, and args if any. Key-value pairs of string string. Can be an array of them
         // hash_map<String, String>
         // output example: /user and key-value pair like <userid: 1>
-
-        Database storage = new Database();
-        HashMap<String, User> hmUserInput = storage.userHashMap;
-        HashMap<String, Posts> hmPostsInput = storage.postsHashMap;
-
-        // HashMap for output
-        HashMap<String[], String[]> hmUserOutput = new HashMap<String[], String[]>();
-        HashMap<String[], String[]> hmPostsOutput = new HashMap<String[], String[]>();
-
-
-        // receive input from Database userHashMap and postsHashMap <String, Object>
-        // Parse the input into a hash_map<String, String>
-        String[] parsingInput, parsingInputTwo;
-        String[] splitUserId;
-        String key, value, parsingInputFirst, parsingInputSecond, parsingSecondFirst, parsingSecondSecond;
-        boolean boolToCheckForParam = false;
-        boolean boolToCheckForArgs = false;
-        String valueForOutput;
-
-        // Loop for User
-        for (Map.Entry m : hmUserInput.entrySet()) {
-
-            key = String.valueOf(m.getKey());
-            value = String.valueOf(m.getValue());
-
-            for (int i = 0; i < value.length(); i++) {
-                if (value.charAt(i) == '?') {
-                    boolToCheckForParam = true;
-                }
-            }
-            if (boolToCheckForParam == true) {
-                parsingInput = value.split("\\?");
-                key = parsingInput[0];
-                //key = key + ": ";
-                value = parsingInput[1];
-                splitUserId = value.split("\\=");
-                value = splitUserId[1];
-                hmUserOutput.put(new String[]{"userid: ", "username: "}, new String[]{value, key});
-            }
-            else {
-                hmUserOutput.put(new String[]{"username: "}, new String[]{key});
-            }
-
-
         }
+    public static ArrayList getUser () {
+        ArrayList<String> alUserOutput = new ArrayList<>();
+        String name,nameOutput;
+        for (User m : userArrayList) {
+            name = String.valueOf(m.getUsername());
+            nameOutput = new StringBuilder().append("username: ").append(name).toString();
+            if (name != "Error") {
+                alUserOutput.add(nameOutput);
+            } else alUserOutput.add("Error");
+        }
+        return alUserOutput;
+    }
+    public static ArrayList getUserbyId() {
+        ArrayList<String[]> alUserOutput = new ArrayList<>();
+        String name, UserIdString, nameOutput, userIdStringOutput;
+        int userId;
 
-        // Loop for Posts
-        boolToCheckForArgs=false;
-        for (Map.Entry m : hmPostsInput.entrySet()) {
+        for (User m : userArrayList) {
+            name = m.getUsername();
+            userId = m.getUserID();
+            UserIdString = String.valueOf(userId);
+            if (name != "Error") {
+                nameOutput = new StringBuilder().append("username: ").append(name).toString();
+                userIdStringOutput = new StringBuilder().append("userid: ").append(UserIdString).toString();
 
-            key = String.valueOf(m.getKey());
-            value = String.valueOf(m.getValue());
-
-            for (int i = 0; i < value.length(); i++) {
-                if (value.charAt(i) == '&') {
-                    boolToCheckForArgs = true;
-                }
-                parsingInput = value.split("\\?");
-                parsingInputFirst = parsingInput[0];
-                parsingInputSecond = parsingInput[1];
-                if (boolToCheckForArgs == true) {
-                    parsingInputTwo = parsingInputSecond.split("\\&");
-                    parsingSecondFirst = parsingInputTwo[0];
-                    parsingSecondSecond = parsingInputTwo[1];
-                    hmPostsOutput.put(new String[]{"userid: ", "postid: ", "data: "}, new String[]{parsingInputFirst, parsingSecondFirst, parsingSecondSecond});
-                }
-                else {
-                    hmPostsOutput.put(new String[]{"userid: ", "postid: "}, new String[]{parsingInputFirst, parsingInputSecond});
-                }
+                String[] arg1 = {userIdStringOutput,nameOutput};
+                alUserOutput.add(arg1);
+            } else {
+                String[] arg2 = {"Error"};
+                alUserOutput.add(arg2);
             }
         }
+        return alUserOutput;
+    }
+    public static ArrayList getPostsById() {
+        ArrayList<String[]> alPostsOutput = new ArrayList<>();
+        String userIdString, postsIdString, userIdStringOutput, postsIdStringOutput;
+        int userId, postsId;
+        for (Posts m : postsArrayList) {
+            postsId = m.getPostID();
+            userId = m.getUserId();
+            postsIdString = String.valueOf(postsId);
+            userIdString = String.valueOf(userId);
+            if (userId != -1) {
+                userIdStringOutput = new StringBuilder().append("userid: ").append(userIdString).toString();
+                postsIdStringOutput = new StringBuilder().append("postid: ").append(postsIdString).toString();
+
+
+                String[] arg1 = {postsIdStringOutput, userIdStringOutput};
+                alPostsOutput.add(arg1);
+            } else {
+                String[] arg2 = {"Error"};
+                alPostsOutput.add(arg2);
+            }
+        }
+        return alPostsOutput;
+    }
+    public static ArrayList getPostsByLength() {
+        ArrayList<String[]> alPostsOutput = new ArrayList<>();
+        String content, userIdString, postsIdString, userIdStringOutput, postsIdStringOutput, contentOutput;
+        int userId, postsId;
+        for (Posts m : postsArrayList) {
+            postsId = m.getPostID();
+            userId = m.getUserId();
+            content = m.getPostContent();
+            postsIdString = String.valueOf(postsId);
+            userIdString = String.valueOf(userId);
+            if (userIdString != "-1") {
+                userIdStringOutput = new StringBuilder().append("userid: ").append(userIdString).toString();
+                postsIdStringOutput = new StringBuilder().append("postid: ").append(postsIdString).toString();
+                contentOutput = new StringBuilder().append("data: ").append(content).toString();
+                String[] arg1 = {postsIdStringOutput, userIdStringOutput, contentOutput};
+                alPostsOutput.add(arg1);
+            } else {
+                String[] arg2 = {"Error"};
+                alPostsOutput.add(arg2);
+            }
+        }
+        return alPostsOutput;
     }
 }
 

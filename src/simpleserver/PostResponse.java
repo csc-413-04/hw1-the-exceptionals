18ceecce5d1891792049226a3ee2212899670a81
Bijan Mahdavi <bmahdavi@mail.sfsu.edu>
2018-10-04 20:28:01 -0700

    tiny bit more

--- src/simpleserver/PostResponse.java
+++ src/simpleserver/PostResponse.java
@@ -1,14 +1,19 @@
 package simpleserver;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 public class PostResponse extends Response {
+
+    ArrayList<String> posts;
 
     public PostResponse(ArrayList<String> postList, int id, int length) { // if length = -1, we assume that length parameters were not specified
 
     }
 
     public void response() {
+        Gson gson = new Gson();
 
     }
 }
